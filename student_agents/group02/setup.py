3dc006b7000b0c6019430fb35876ff562dba6993
Schwarcz Florian <florian.schwarcz@gmx.at>
2023-10-09 11:10:06 +0200

    Created group folder

---
+++ student_agents/group02/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='group02',
+    version='',
+    packages=find_packages(),
+    package_data={'group02': ["resources/*"]},
+    decription="Installation of the agent",
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='',
+    zipsafe=True
+)
